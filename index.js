02f993f45bba4e8e5aab3dadacd7daa1cbcc6977
jeffrey <projectsupremebot@gmail.com>
2020-05-20 01:41:53 +0000

    Archivo principal del proyecto

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+
+let { readdirSync } = require('fs');
+
+client.comandos = new Discord.Collection();
+
+for(const file of readdirSync('./comandos/')){
+  if(file.endsWith(".js")){
+  let fileName = file.substring(0, file.length -3);
+  let fileContents = require(`./comandos/${file}`);
+  client.comandos.set(fileName, fileContents);
+  delete require.cache[require.resolve(`./comandos/${fileName}.js`)];
+  }
+}
+for(const file of readdirSync('./eventos')){
+  if(file.endsWith(".js")){
+  let fileName = file.substring(0, file.length -3);
+  let fileContents = require(`./eventos/${file}`);
+  client.on(fileName, fileContents.bind(null, client));
+  delete require.cache[require.resolve(`./eventos/${file}`)];
+  }
+}
+
+
+
+client.login(config.token)
+    .then(() => {
+        console.log(`${client.user.tag} ha iniciado correctamente.`);
+    })
+    .catch((err) => {
+        console.error('Error al iniciar sesión: '+err);
+    });
+
