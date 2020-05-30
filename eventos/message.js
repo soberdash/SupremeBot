f2b001b7180a6f17367cbce89839c9281a306317
jeffrey <projectsupremebot@gmail.com>
2020-05-30 21:11:22 +0000

    Cambios en setTimeout

--- eventos/message.js
+++ eventos/message.js
@@ -1,16 +1,5 @@
 module.exports = (client, message) => {
-  const prefix = '>'
-  const config = require('/home/jeffrey/sb/config')
-  const mongoose = require('mongoose')
-  const Discord = require('discord.js')
-	if(message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) {
-    message.channel.send('<:supreme:690641733641306223> Mi prefix en el servidor ``'+message.guild.name+'`` es: ``'+prefix+'``').then((msg) => {
-      setTimeout(() => {
-        message.delete();
-        msg.delete();
-      }, 7000)
-    })
-  }
+  const prefix = ">"
   if (!message.content.startsWith(prefix)) return
   if (message.author.bot) return
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
