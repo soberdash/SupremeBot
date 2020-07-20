6fa7d97cd93f27e77daba5d0b6c1bff8fc658be8
Jeffrey <51806683+JeffreyDeveloper@users.noreply.github.com>
2020-07-19 19:36:37 -0600

    Delete logs.js

--- models/logs.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const logsSchema = new mongoose.Schema({
-    guildID: String,
-    channelID: String
-})
-
-module.exports = mongoose.model('Logs', logsSchema)
