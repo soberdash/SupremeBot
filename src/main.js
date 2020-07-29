16af3a2f45bb55202232af163089e3c89b8f8f95
JeffreyDeveloper <dev@correo.supremeproject.me>
2020-07-29 18:52:02 +0000

    Minimal changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const mongoose = require("mongoose");
-const data = require("../utils/data.json");
+const data = require("./utils/data.json");
 
 /*
 const DBL = require("dblapi.js")
